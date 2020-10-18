857a1d1cb150f52a9db8fd83e72f6988abbc7991
Hicharles Rocha <hicharlesolvr@gmail.com>
2020-10-18 15:08:56 -0300

    Finishing lesson 3

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -52,7 +52,7 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends,
+      open_on_weekends: open_on_weekends === 'true',
       images
     };
 
@@ -69,7 +69,7 @@ export default {
           path: Yup.string().required()
       })
       )
-    })
+    });
 
     await schema.validate(data, {
       abortEarly: false,
